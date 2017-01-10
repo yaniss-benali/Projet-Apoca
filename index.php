3adc1da08a89fa0f9659e925c19fbcad71056a53
Doukelli <Doukelli@LDOUKELLI-1997.home>
2017-01-10 16:11:28 +0100

    Installation de bootstrap. Intégration de boostrap a index.php. Installation et intégration de glyphicon personnalisé.

--- index.php
+++ index.php
@@ -1,9 +1,16 @@
 <!DOCTYPE HTML>
 <html>
   <head>
+      <link href="bootsrap/css/bootstrap.min.css" rel="stylesheet">
+      <link href="bootsrap/ico/demo-files/demo.cs" rel="stylesheet">
+      <link href="bootsrap/ico/style.css" rel="stylesheet">
+      <script src="bootsrap/js/bootstrap.min.js" ></script>
+      <script src="bootsrap/js/bootstrap.js" ></script>
+      <script src="bootsrap/ico/demo-files/demo.js" ></script>
+      
     <title>OpenLayers Demo</title>
     <style type="text/css">
-      html, body, #basicMap {
+      html, body, #basicMap {site
           width: 100%;
           height: 100%;
           margin: 0;
@@ -21,6 +28,31 @@
             new OpenLayers.Projection("EPSG:900913") // en projection Mercator sphérique
           ), 15 // Zoom level
         );
+    //Création d'un layer qui va contenir des marqueurs
+    var markers = new OpenLayers.Layer.Markers( "Markers" );
+    map.addLayer(markers);
+
+    // creation d'un marqueur aux coordonnées longlat1 et transformation en coord de projection
+
+    var lonLat1 = new OpenLayers.LonLat(2.352684100000033,48.856924)
+    .transform(
+    new OpenLayers.Projection("EPSG:4326"),
+    map.getProjectionObject()
+    );
+
+    //creation d'un marqueur icone     
+    var size = new OpenLayers.Size(30,30);
+    var offset = new OpenLayers.Pixel(-(size.w/2), -size.h);
+    var icon = new OpenLayers.Icon('../icon/mystere.png', size, offset);
+    marker = new OpenLayers.Marker(lonLat1,icon);
+
+    //ajout du marker      
+    markers.addMarker(marker);
+
+    //le marqueur devient intéractif: click : PC uniquement !
+    marker.events.register("click", marker,cliquemarker);  
+    //click ne marche pas sous openlayers sur mobile alors on met touchstart....
+    marker.events.register("touchstart", marker,cliquemarker); 
       }
     </script>
   </head>
